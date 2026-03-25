0960945257c0794401a5eaac0a61b6ba103ab319
Campocruz <cosmacampo93@yahoo.it>
2026-03-25 15:43:29 +0100

    function work but not in the page

--- script.js
+++ script.js
@@ -42,38 +42,54 @@ const containerCardEl = document.getElementById('card-row');
 // select DOM node from query form
 const formEl = document.querySelector('form');
 // select DOM node by id for all input
-const inmputRole = document.getElementById('inputRole');
 const inmputName = document.getElementById('inputName');
+const inmputRole = document.getElementById('inputRole');
 const inmputEmail = document.getElementById('inputEmail');
 const inmputImg = document.getElementById('inputImg');
 
+// wait the event submit
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-
+  // read and add new member in array list
+  const newMember = {
+    name: inmputName.value,
+    role: inmputRole.value,
+    email: inmputEmail.value,
+    img: inmputImg.value
+  };
+  teamMembers.unshift(newMember);
+  // render new array
+  renderCard(teamMembers);
 })
 
-for (let i = 0; i < teamMembers.length; i++) {
-  // create new DOM object 
-  const memberCard = document.createElement('div');
-  // add class at the object
-  memberCard.classList.add('col');
-  // complete the info every iteraction
-  memberCard.innerHTML = `
+// Render member at first page load
+renderCard(teamMembers);
+
+// Function to render the member card
+function renderCard(listMembers) {
+  for (let i = 0; i < listMembers.length; i++) {
+    // create new DOM object 
+    const memberCard = document.createElement('div');
+    // add class at the object
+    memberCard.classList.add('col');
+    // complete the info every iteraction
+    memberCard.innerHTML = `
     <div class="card mb-3" style="max-width: 340px;">
       <div class="row g-0">
         <div class="col-md-4">
-          <img src="./assets/${teamMembers[i].img}" class="img-fluid rounded-start" alt="${teamMembers[i].img}">
+          <img src="./assets/${listMembers[i].img}" class="img-fluid rounded-start" alt="${listMembers[i].img}">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <p class="card-title text-uppercase">${teamMembers[i].name}</p>
-            <p class="card-text">${teamMembers[i].role}</p>
-            <p class="card-text">${teamMembers[i].email}</p>
+            <p class="card-title text-uppercase">${listMembers[i].name}</p>
+            <p class="card-text">${listMembers[i].role}</p>
+            <p class="card-text">${listMembers[i].email}</p>
           </div>
         </div>
       </div>
     </div>
   `;
-  // insert new object in a card-row
-  containerCardEl.appendChild(memberCard);
+    // insert new object in a card-row
+    containerCardEl.appendChild(memberCard);
+  }
 }
